3d5148f4fa6cc7351124c63c4aac5a9589f00ec2
jingyi.liu <m18768120089@163.com>
2023-07-19 17:31:40 +0800

    modify

--- pt/js/bundle.9f61c4b3cbf336f23f4f08a571a4365435f5e050f84f17de9bebe0f29be3b253b62bb6038a990a9ddea1a63c8f1b9972cf57b38e8fde1f55c036a41924beda6d.js
+++ pt/js/bundle.9f61c4b3cbf336f23f4f08a571a4365435f5e050f84f17de9bebe0f29be3b253b62bb6038a990a9ddea1a63c8f1b9972cf57b38e8fde1f55c036a41924beda6d.js
@@ -10,7 +10,7 @@ const imageAltClass = 'img_alt';
 
 
 const defaultSiteLanguage = 'en';
-const baseURL = 'https://sophieforai.github.io/';
+const baseURL = 'https://www.sophieforai.github.io/';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
 const goBackClass = 'button_back';
